c73cb0a80c6e878ea9f83fcb5054f3ac5c1058f5
ar78ilnik <arakchiev.ilya@yandex.ru>
2022-01-24 16:23:02 +0300

    Chack bid

--- app/js/script.js
+++ app/js/script.js
@@ -4,7 +4,10 @@
         menuClose = document.querySelector('.menu__close'),
         popup = document.querySelector('.container_popup'),
         bodyBack = document.querySelector('.body__back'),
-        footerDet = document.querySelectorAll('.footer__det');
+        footerDet = document.querySelectorAll('.footer__det'),
+        linkBlueAbout = document.querySelector('.link_blue-about'),
+        bidBlock = document.querySelector('.bid'),
+        bidClose = document.querySelector('.bid__close');
 
     menuButton.addEventListener('click', () => {
         let expanded = menuButton.getAttribute('aria-expanded') === 'true' || false;
@@ -22,9 +25,16 @@
         document.body.style.overflow = '';
     })
 
+    linkBlueAbout.addEventListener('click', () => {
+        bidBlock.classList.toggle('bid_open');
+    })
+
+    bidClose.addEventListener('click', () => {
+        bidBlock.classList.toggle('bid_open');
+    })
+
     bodyBack.addEventListener('click', (event) => {
         if (event.target === bodyBack) {
-            console.log('1');
             let expanded = menuButton.getAttribute('aria-expanded') === 'true' || false;
             menuButton.setAttribute('aria-expanded', !expanded);
             popup.classList.toggle('container_popup-open');
